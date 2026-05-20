c98b0ba4c80164b46c29fe86076f1712ee28d468
Chidi Offor <chidioffor@yahoo.com>
2026-05-20 16:14:33 +0100

    Configure GitHub Pages deployment
    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
+  base: '/Document_Ledger_2/',
+  // server: { port: 5173 },
 });
